76532b54198b46c080cad956e1e32ef4d4dc3ae7
VarunSingh19 <varunsingh18@icloud.com>
2024-09-09 01:07:58 +0530

    deployment

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
-import os
+from urllib.parse import quote
 
 class Config:
-    SQLALCHEMY_DATABASE_URI ='postgresql://postgres:123456@localhost:5433/stocks_db'
+    # URL-encode the password if it has special characters
+    password = quote("Varunsingh@24")  # Adjust the password with special characters here
+
+    # Correct connection string with encoded password
+    SQLALCHEMY_DATABASE_URI = f"postgresql://postgres.xodcbschrhenyzmcmfao:{password}@aws-0-ap-south-1.pooler.supabase.com:6543/postgres"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
