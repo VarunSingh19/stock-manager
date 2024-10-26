b2a75bc64a8529986ed5af51997ff9b94b6f2b93
Varun Singh <104829215+VarunSingh19@users.noreply.github.com>
2024-10-27 01:08:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,8 @@ from urllib.parse import quote
 
 class Config:
     password = quote("Varunsingh@24")  
-    SQLALCHEMY_DATABASE_URI = f"postgresql://postgres.xodcbschrhenyzmcmfao:{password}@aws-0-ap-south-1.pooler.supabase.com:6543/postgres"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = f"postgresql://postgres.tbqkuipwwxyacrmwgjoa:{password}@aws-0-ap-south-1.pooler.supabase.com:6543/postgres"
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+# Varunsingh@24
+# postgresql://postgres.agswpvhgwvqibovhfxaa:[YOUR-PASSWORD]@aws-0-ap-south-1.pooler.supabase.com:6543/postgres
